079323400818d5b6c3029584dcea3ff9cac36bd4
GUSTAVO JOÃO CARDOSO <gustavo_j_cardoso@estudante.sesisenai.org.br>
2026-04-13 14:57:48 -0300

    feat: funções createUser e FindUserByEmail criadas

--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -1,11 +1,34 @@
-import db from "../database/db";
-import bcrypt from 'bcryptjs';
+import db from "../database/db.js";
 
-async function createUser(nome, email, senha) {
+async function createUser(nome, email, senhaHash) {
+  try {
+    //Convertendo a seha em hase
+    const query = `
+        INSERT INTO usuarios (nome, email, senha)
+        VALUES ($1, $2, $3)
+        RETURNING id, nome, email;
+        `;
+        const values = [nome, email, senhaHash];
+        const result = await db.query(query, values);
+        return result.rows[0];
+
+  } catch (error) {
+    console.log(error);
+    return (error)
+  }
+}
+
+async function findUserByEmail(email) {
     try {
-        //Convertendo a seha em hase
-        const senhaHash = await bcrypt.hash(senha, 10);
+        const result = await db.query(
+            'SELECT * FROM usuarios WHERE email = $1',
+            [email]
+        );
+        return result.rows[0];
     } catch (error) {
-        
+        console.log(error);
+        return (error)
     }
 }
+
+export { createUser, findUserByEmail };
